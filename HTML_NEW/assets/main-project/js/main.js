9c338bcbb66e1ba8904febfa9b72b71517fb5558
Thien <thien@ThienPhams-MacBook-Air.local>
2018-04-09 10:11:35 +0700

    update ks-booking page html

--- HTML_NEW/assets/main-project/js/main.js
+++ HTML_NEW/assets/main-project/js/main.js
@@ -34,4 +34,7 @@ $(document).ready(function() {
             scrollTop : 0                       // Scroll to top of body
         }, 500);
     });
+
+    // ===== Tooltip cho phần giá tổng ==== 
+    $('[data-toggle="tooltip"]').tooltip();
 });
