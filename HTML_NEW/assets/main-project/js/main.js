5beb66a49588c216a9af89f6234a9bf0483713bd
Thien <thien@ThienPhams-MacBook-Air.local>
2018-04-10 14:13:49 +0700

    update feedback html

--- HTML_NEW/assets/main-project/js/main.js
+++ HTML_NEW/assets/main-project/js/main.js
@@ -37,4 +37,26 @@ $(document).ready(function() {
 
     // ===== Tooltip cho phần giá tổng ==== 
     $('[data-toggle="tooltip"]').tooltip();
+
+    // ===== Slide Section Đối Tác ==== 
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+            pauseOnHover: false,
+            responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
 });
