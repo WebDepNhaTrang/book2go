a533d8c40d113ac99b3e645846b2442c624d7600
Thien <thien@ThienPhams-MacBook-Air.local>
2018-04-18 23:48:47 +0700

    update footer + social cms

--- public/themes/book2go/functions/functions.php
+++ public/themes/book2go/functions/functions.php
@@ -11,10 +11,12 @@ register_page_template([
 ]);
 
 register_post_template([
+    'default-post-template' => 'Default Template',
     'news-detail-template' => 'News Detail Template'
 ]);
 
 register_category_template([
+    'default-category-template' => 'Default Template',
     'news-list-template' => 'News List Template'
 ]);
 
@@ -66,6 +68,136 @@ theme_option()->setField([
         ]
     ],
     'helper' => __('Copyright on footer of site'),
+]);
+
+theme_option()->setSection([
+    'title' => __('Footer'),
+    'desc' => __('Footer custom'),
+    'id' => 'opt-text-subsection-footer',
+    'subsection' => true,
+    'icon' => 'fa fa-home',
+]);
+theme_option()->setField([
+    'id' => 'footer_block1',
+    'section_id' => 'opt-text-subsection-footer',
+    'type' => 'textarea',
+    'label' => __('Block 1'),
+    'attributes' => [
+        'name' => 'footer_block1',
+        'value' => '<h6 class="text-uppercase mb-4 font-weight-bold">Book2govn</h6>
+        <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>',
+        'options' => [
+            'class' => 'form-control'
+        ]
+    ]
+]);
+theme_option()->setField([
+    'id' => 'footer_block2',
+    'section_id' => 'opt-text-subsection-footer',
+    'type' => 'textarea',
+    'label' => __('Block 2'),
+    'attributes' => [
+        'name' => 'footer_block2',
+        'value' => '<h6 class="text-uppercase mb-4 font-weight-bold">Sản Phẩm</h6>
+        <p><a href="#!">MDBootstrap</a></p>
+        <p><a href="#!">MDWordPress</a></p>
+        <p><a href="#!">BrandFlow</a></p>
+        <p><a href="#!">Bootstrap Angular</a></p>',
+        'options' => [
+            'class' => 'form-control'
+        ]
+    ]
+]);
+theme_option()->setField([
+    'id' => 'footer_block3',
+    'section_id' => 'opt-text-subsection-footer',
+    'type' => 'textarea',
+    'label' => __('Block 3'),
+    'attributes' => [
+        'name' => 'footer_block3',
+        'value' => '<h6 class="text-uppercase mb-4 font-weight-bold">Liên Kết</h6>
+        <p><a href="#!">Your Account</a></p>
+        <p><a href="#!">Become an Affiliate</a></p>
+        <p><a href="#!">Shipping Rates</a></p>
+        <p><a href="#!">Help</a></p>',
+        'options' => [
+            'class' => 'form-control'
+        ]
+    ]
+]);
+theme_option()->setField([
+    'id' => 'footer_block4',
+    'section_id' => 'opt-text-subsection-footer',
+    'type' => 'textarea',
+    'label' => __('Block 4'),
+    'attributes' => [
+        'name' => 'footer_block4',
+        'value' => '<h6 class="text-uppercase mb-4 font-weight-bold">Liên Hệ</h6>
+        <p><i class="fa fa-home mr-3"></i> Nha Trang City, Khanh Hoa Province, Vietnam</p>
+        <p><i class="fa fa-envelope mr-3"></i> info@book2govn.com</p>
+        <p><i class="fa fa-phone mr-3"></i> (+84) 974 974 567</p>',
+        'options' => [
+            'class' => 'form-control'
+        ]
+    ]
+]);
+
+theme_option()->setSection([
+    'title' => __('Social'),
+    'desc' => __('Social Link'),
+    'id' => 'opt-text-subsection-social',
+    'subsection' => true,
+    'icon' => 'fa fa-home',
+    'fields' => [
+        [
+            'id' => 'social_facebook',
+            'type' => 'text',
+            'label' => __('Facebook'),
+            'attributes' => [
+                'name' => 'social_facebook',
+                'value' => '#',
+                'options' => [
+                    'class' => 'form-control'
+                ]
+            ],
+        ],
+        [
+            'id' => 'social_twitter',
+            'type' => 'text',
+            'label' => __('Twitter'),
+            'attributes' => [
+                'name' => 'social_twitter',
+                'value' => '#',
+                'options' => [
+                    'class' => 'form-control'
+                ]
+            ],
+        ],
+        [
+            'id' => 'social_google',
+            'type' => 'text',
+            'label' => __('Google'),
+            'attributes' => [
+                'name' => 'social_google',
+                'value' => '#',
+                'options' => [
+                    'class' => 'form-control'
+                ]
+            ],
+        ],
+        [
+            'id' => 'social_linkedin',
+            'type' => 'text',
+            'label' => __('LinkedIn'),
+            'attributes' => [
+                'name' => 'social_linkedin',
+                'value' => '#',
+                'options' => [
+                    'class' => 'form-control'
+                ]
+            ],
+        ],
+    ],
 ]);
 
 theme_option()->setArgs(['debug' => true]);
