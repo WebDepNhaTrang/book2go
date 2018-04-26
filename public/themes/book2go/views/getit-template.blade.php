66bbff1dfb538e2c76cb95b699b7cb3a3c53594a
Thien <thien@ThienPhams-MacBook-Air.local>
2018-04-26 11:06:20 +0700

    update getit-page

--- public/themes/book2go/views/getit-template.blade.php
+++ public/themes/book2go/views/getit-template.blade.php
@@ -17,20 +17,6 @@ if(!empty($page)) $post = $page; @endphp
             </nav>
         </div>
     </div>
-    
-    <!--form tìm kiếm tour và khách sạn - căn hộ-->
-    <div class="container">
-            <div class="promo-caption my-4">
-                <form action="#" method="post">
-                    <select class="form-control text-md-right search-select" name="searchGetIt">
-                        <option value="ks-ch">Khách sạn - Căn hộ</option>
-                        <option value="tour">Tour</option>
-                    </select>
-                    <input type = "submit" class="btn btn-warning" value="Tìm kiếm"/>
-                </form>
-            </div>
-        </div>
-    <!--end form tìm kiếm tour và khách sạn - căn hộ-->
 
     <div class="container">
         <div class="promo-caption text-center my-4">
@@ -44,31 +30,84 @@ if(!empty($page)) $post = $page; @endphp
     </div>
     @php
         $data = get_promotions_front();
-
     @endphp
     <div class="container">
-        <div class="row news-list">
-            @foreach($data as $v)
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                <div class="block-article">
-                    <div class="block-image">
-                        <img class="img-fluid" src="{{get_object_image($v->image, 'featured')}}" alt="Image">
-                        <div class="like">
-                            <div>- {{ $v->cost }}%</div>
+        
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Khách sạn - Căn hộ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Tour</a>
+            </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                <div class="row news-list">
+                    @foreach($data as $v)
+                    @php
+                        //echo "<pre>";
+                        //print_r($v->tours->toArray());
+                        //echo "</pre>";
+                        //die();
+                    @endphp
+                    @if($v->hotels || $v->apartments)
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="block-article">
+                            <div class="block-image">
+                                <img class="img-fluid" src="{{get_object_image($v->image, 'featured')}}" alt="Image">
+                                <div class="like">
+                                    <div>- {{ $v->cost }}%</div>
+                                </div>
+                            </div>
+                            <h5 class="block-description">						
+                                <a class="mod-articles-category-title " href="{{route('public.single', $v->hotels[0]->slug)}}">
+                                    {{ $v->name }}
+                                </a>
+                            </h5>
+                            <div class="date">Từ {{date('d/m/Y', strtotime($v->start_date)) }} đến {{date('d/m/Y', strtotime($v->end_date)) }}</div>
                         </div>
                     </div>
-                    <h5 class="block-description">						
-                        <a class="mod-articles-category-title " href="{{route('public.single', $v->hotels[0]->slug)}}">
-                            {{ $v->name }}
-                        </a>
-                    </h5>
-                    <div class="date">Từ {{date('d/m/Y', strtotime($v->start_date)) }} đến {{date('d/m/Y', strtotime($v->end_date)) }}</div>
+                    @endif
+                    @endforeach
+                </div>
+                <div class="btn-block text-center">
+                    <a class="btn btn-default btn-outline" href="">Xem thêm</a>
                 </div>
             </div>
-            @endforeach
-        </div>
-        <div class="btn-block text-center">
-            <a class="btn btn-default btn-outline" href="">Xem thêm</a>
+            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+            <div class="row news-list">
+                    @foreach($data as $v)
+                    @php
+                        //echo "<pre>";
+                        //print_r($v->tours->toArray());
+                        //echo "</pre>";
+                        //die();
+                    @endphp
+                    @if($v->tours)
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="block-article">
+                            <div class="block-image">
+                                <img class="img-fluid" src="{{get_object_image($v->image, 'featured')}}" alt="Image">
+                                <div class="like">
+                                    <div>- {{ $v->cost }}%</div>
+                                </div>
+                            </div>
+                            <h5 class="block-description">						
+                                <a class="mod-articles-category-title " href="{{route('public.single', $v->hotels[0]->slug)}}">
+                                    {{ $v->name }}
+                                </a>
+                            </h5>
+                            <div class="date">Từ {{date('d/m/Y', strtotime($v->start_date)) }} đến {{date('d/m/Y', strtotime($v->end_date)) }}</div>
+                        </div>
+                    </div>
+                    @endif
+                    @endforeach
+                </div>
+                <div class="btn-block text-center">
+                    <a class="btn btn-default btn-outline" href="">Xem thêm</a>
+                </div>
+            </div>
         </div>
     </div>
 </section>
