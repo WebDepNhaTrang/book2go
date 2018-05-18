cc46b1072d0efe1889761baf4d389b97c3be3335
Anh Ngo <nivianh@gmail.com>
2018-05-18 21:01:47 +0700

    fix bug 18/05

--- plugins/servicer/src/Repositories/Eloquent/PromotionRepository.php
+++ plugins/servicer/src/Repositories/Eloquent/PromotionRepository.php
@@ -65,7 +65,24 @@ class PromotionRepository extends RepositoriesAbstract implements PromotionInter
 	    	                ->orderBy('promotions.cost', 'desc');
             }
 		}else{
-     	    
+     	    $data = $this->model
+     	    			->join('promotion_servicer', 'promotion_servicer.promotion_id', '=', 'promotions.id')
+     	    			->join('servicers', 'promotion_servicer.reference_id', '=', 'servicers.id')
+     	    			->where('promotion_servicer.reference_id', $id)
+    					->where('promotion_servicer.reference', $type)
+    					->where('promotions.status', '=', 1)
+    					->where(function ($query) use ($checkin, $checkout) {
+    					     $query->where(function ($query) use ($checkin) {
+    					                     $query->whereDate('promotions.start_date', '<=' ,$checkin)
+    					                           ->whereDate('promotions.end_date', '>=' ,$checkin);
+    					         })->orWhere(function ($query)  use ($checkin, $checkout) {
+    					                     $query->whereDate('promotions.start_date', '>=' ,$checkin)
+    					                           ->whereDate('promotions.start_date', '<=' ,$checkout);
+    					         });
+    					 })
+    					->select(['*', 'promotions.name as promotion_name'])
+    					->distinct()
+    	                ->orderBy('promotions.cost', 'desc');
 		}
  	    
 		$data = apply_filters(BASE_FILTER_BEFORE_GET_FRONT_PAGE_ITEM, $data, $this->model, PROMOTION_MODULE_SCREEN_NAME)->first();
